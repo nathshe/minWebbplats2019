f1f44ef99e4d7a3546d713d10d928235dc72ea7f
nathshe <35328338+nathshe@users.noreply.github.com>
2019-05-25 14:18:13 +0200

    Add files via upload
    First upload

---
+++ webSites/JS/script.js
@@ -0,0 +1,15 @@
+function click1() {
+  document.getElementById("userReq1").style.display = "flex";
+  document.getElementById("button1").style.display = "none";
+  document.getElementById("button2").style.display = "block";
+}
+
+function click2() {
+  document.getElementById("userReq2").style.display = "flex";
+  document.getElementById("button2").style.display = "none";
+  document.getElementById("button3").style.display = "block";
+}
+
+function click3() {
+  document.getElementById("userReq3").style.display = "flex";
+}
